9c229e24b4661e4f3011a28602e936aac9926b86
yangtenghui <15817257525@163.com>
2020-03-29 23:30:59 +0800

    update

--- gin-study/chapter01/main.go
+++ gin-study/chapter01/main.go
@@ -1,8 +1,18 @@
 package main
+
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println(123)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"code":200,
+			"message": "pong",
+			"data":"123",
+		})
+	})
+	r.Run(":8081") // listen and serve on 0.0.0.0:8080
+
 }
