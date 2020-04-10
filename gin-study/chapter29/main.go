6f86398a389917da94779d558c7e785d581c2eaf
yangtenghui <yangtenghui@starduststudio.com>
2020-04-10 18:26:54 +0800

    'task:map切片复习'

--- gin-study/chapter29/main.go
+++ gin-study/chapter29/main.go
@@ -14,4 +14,20 @@ func main()  {
  var p4 person
  fmt.Println(p4)
 
+
+//使用键值对初始化
+ p5 :=person{
+ 	name:"xiaowangzi",
+ 	city:"beijing",
+ 	age:12,
+ }
+ fmt.Println(p5)
+
+ //也可以对结构体指针进行键值对初始化，例如
+	p6 := &person{
+		name: "小王子",
+		city: "北京",
+		//age:  18,
+	}
+	fmt.Println(p6)
 }
---
+++ gin-study/chapter30/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type student struct {
+	name string
+	age  int
+}
+
+func main() {
+	slice := []int{0, 1, 2, 3}
+	myMap :=make(map[int]*int)
+
+	for index,value:=range slice{
+		num := value
+		fmt.Println(num)
+		myMap[index] = &num
+	}
+	fmt.Println("=====new map=====")
+	fmt.Println(myMap)
+	//prtMap(myMap)
+}
+
+
+func prtMap(myMap map[int]*int) {
+	for key, value := range myMap {
+		fmt.Printf("map[%v]=%v\n", key, *value)
+	}
+}
